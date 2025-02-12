52d7327d05b90d88eddc498b8b04bee951b02b45
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-02-12 19:56:44 +0200

    edit main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,13 +4,13 @@ const inputs = Array.from(document.querySelectorAll('input'));
 
 function addLabelToInputs(arrayOfInputs) {
   arrayOfInputs.forEach((input) => {
-    const parent = input.parentNode;
+    const parentElement = input.parentNode;
     const labelElement = document.createElement('label');
 
     labelElement.classList.add('field-label');
     labelElement.textContent = input.name;
     labelElement.setAttribute('for', `${input.id}`);
-    parent.prepend(labelElement);
+    parentElement.prepend(labelElement);
 
     input.setAttribute(
       'placeholder',
