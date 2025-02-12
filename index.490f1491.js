faa16bc86004f3ac8ff4b4903ac79858d8e26c47
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-02-12 19:51:54 +0200

    make build

---
+++ index.490f1491.js
@@ -0,0 +1,2 @@
+!function(e){e.forEach(function(e){var t=e.parentNode,n=document.createElement("label");n.classList.add("field-label"),n.textContent=e.name,n.setAttribute("for","".concat(e.id)),t.prepend(n),e.setAttribute("placeholder","".concat(e.name[0].toUpperCase()+e.name.slice(1)))})}(Array.from(document.querySelectorAll("input")));
+//# sourceMappingURL=index.490f1491.js.map
